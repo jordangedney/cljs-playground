19397e9e51aff7f3fc1df90d5a425e70cbd628e6
Jordan Gedney <jjgedney@mtu.edu>
2018-04-02 22:46:46 -0400

    Alternating Xs and Os

--- src/cljs_playground/components.cljs
+++ src/cljs_playground/components.cljs
@@ -3,66 +3,34 @@
           [clojure.string :as s]
           [cljs-playground.macros :refer-macros [with-source-code]]))
 
-(defn next-piece [piece]
-  (case piece
-    "X" "O"
-    "O" "."
-    "X"))
 
-(defn toggle-piece [index board]
-  (swap! board update-in [:game-board] #(assoc % index (next-piece (nth % index)))))
-;  (update-in board [:game-board] (fn [b] (js/alert b)
-;                                   (assoc b index (next-piece (nth b index))))))
+(defonce whos-turn (atom {:player-x true}))
 
 
-(with-source-code
-  (defn playground [data]
-    (html
-     [:div.outer [:div.middle [:div.inner [:div.content
-        ; [:table.board
-        ;  (for [x (range 1 4)]
-        ;    [:tr (for [y (range 1 4)]
-        ;           [:td [:a {:href "#"
-        ;                     :onClick #(js/alert (str x " " y))}
-        ;                 "."]])])
-
-          [:div.grid-container
-           (for [x (range 0 9)]
-             [:a {:href "#"
-                  :onClick #(toggle-piece x data)}
-             [:div.grid-item (nth (:game-board @data) x) ]])
-           ]
-
-          [:br]
-
-          [:div.button
-          [:input {:type "submit"
-                   :onClick #(reset! data {:game-board (:blank-board @data)
-                                           :blank-board (:blank-board @data)})
-                   :value "Reset" }]
-           ]
-
-          ]]]])))
+(defn update-piece [index board]
+  (let [piece-symbol (if (:player-x @whos-turn) "X" "O")
+        square-is-unplayed (= (nth (:game-board @board) index) ".")]
+    (if square-is-unplayed
+      (do (swap! board update-in [:game-board] #(assoc % index piece-symbol))
+          (swap! whos-turn update-in [:player-x] not)))))
 
 
-                                        ;(reset! app-state {:likes 100})
+(defn my-center-box [& page-content]
+  (html [:div.outer [:div.middle [:div.inner [:div.content page-content]]]]))
 
-; [:h1 (:likes @data) " shades of grey"]
-; [:div
-;
-;  [:br]
-;
-;  [:h1 (:game-board @data)]
-;
-;  [:a {:href "#"
-;       :onClick #(swap! data update-in [:likes] dec)}
-;   "Thumbs down"]
-;
-;  [:ul (for [n (range 1 10)]
-;         [:li {key n} n])]
-;
-;  [:input {:type "submit"
-;           :onClick #(reset! data {:likes 0})
-;           :value "Reset" }]
 
-;  [:br]
+(defn playground [data]
+  (my-center-box
+   [:div.grid-container
+    (for [x (range 0 9)]
+      [:a {:href "#"
+           :onClick #(update-piece x data)}
+       [:div.grid-item (nth (:game-board @data) x) ]])]
+
+   [:br]
+
+   [:div.button
+    [:input {:type "submit"
+             :onClick #(reset! data {:game-board (:blank-board @data)
+                                     :blank-board (:blank-board @data)})
+             :value "Reset" }]]))
